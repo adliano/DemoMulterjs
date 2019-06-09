cdcbcc59237090a3cc3a193366bbf54b34171b8b
Adliano Alves <adliano@me.com>
2019-06-09 00:35:38 -0700

    install npm pcks

---
+++ index.js
@@ -0,0 +1,8 @@
+// Adriano Alves
+// Jun 09 2019
+// Demo code using Multer js
+// https://www.npmjs.com/package/multer
+// run;
+// npm init -y
+// npm install express
+// npm install --save multer
